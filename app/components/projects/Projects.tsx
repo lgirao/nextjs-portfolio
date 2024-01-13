35267bfdbc1f164f183cddb53cd8ec39d3f1f648
lgirao <lesleygirao@gmail.com>
2024-01-13 14:20:29 -0500

    project section start

---
+++ app/components/projects/Projects.tsx
@@ -0,0 +1,12 @@
+const Projects = () => {
+    return (
+        <section className="py-16 bg-slate-100 w-full">
+            <div className="text-center mb-12">
+                <h1 id="projects" className="text-3xl font-bold mb-12 relative heading-after">Projects</h1>
+                <p className="text-lg max-w-2xl mx-auto my-0">Here you will find a showcase of my best projects, personal and professional, using a variety of technology.</p>
+            </div>
+        </section>
+    )
+}
+
+export default Projects;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from './components/hero/Hero';
 import About from './components/about/About';
+import Projects from './components/projects/Projects';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
       <Hero />
       <About />
+      <Projects />
     </main>
   )
 }
